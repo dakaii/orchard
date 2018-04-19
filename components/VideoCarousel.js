106dbe0c5b2ee9cf9b89c30cdb155e4b19dd54a4
dakaii <daiki815@gmail.com>
2018-04-19 22:44:17 +0800

    swiper added

--- components/VideoCarousel.js
+++ components/VideoCarousel.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { StyleSheet, Text, View, Dimensions, WebView }from 'react-native';
-import ScrollView from 'react-native-snap-carousel';
+import Swiper from 'react-native-swiper';
 
 
 class VideoCarousel extends Component {
+  
+  render () {
+    const videos = this.props.videos;
 
-    render () {
-      let videoId = '';
-      if (this.props.selectedVideo) {
-        videoId = this.props.selectedVideo.id.videoId;
-      } else {
+    function produceVideoList (v) {
         return (
-          <Text>...Loading</Text>
+          v.filter(function(video) {
+            return video
+          }).map(function(video) { return (
+            <WebView
+              source={{ uri: `https://www.youtube.com/embed/${video.id.videoId}` }}
+              javaScriptEnabled={true}
+              style={styles.video}
+              key={video.id.videoId}
+            />
+          )})
         )
-      }
-      return (
-        <View style={styles.container}>
-          {/* <ScrollView onSnapToItem={this.setState({ selectedVideo: this.props.videos[3] })}> */}
-          <WebView
-            source={{ uri: `https://www.youtube.com/embed/${videoId}`}}
-            javaScriptEnabled={true}
-            style={styles.window}
-          />
-          {/* </ScrollView> */}
-        </View>
-      );
     }
+
+    function createVideoViews() {
+      if (videos.length) {
+        return (
+          <Swiper showsButtons={true} loop={false}>
+            {produceVideoList(videos)}
+          </Swiper> 
+        );
+      }
+    }
+
+    return (
+      <View style={styles.container}>
+          {createVideoViews()}
+      </View>
+    );
   }
+}
 
   function mapStateToProps(state) {
     return { videos: state.videos,
@@ -64,3 +77,26 @@ const styles = StyleSheet.create({
     height : Dimensions.get('window').height,
   },
 });
+
+// class VideoCarousel extends Component {
+
+//   render () {
+//       if (this.props.selectedVideo) {
+//       videoId = this.props.selectedVideo.id.videoId;
+//       } else {
+//       return (
+//           <Text>...Loading</Text>
+//       )
+//       }
+//       const uri = `https://www.youtube.com/embed/${this.props.selectedVideo.id.videoId}`;
+//       return (
+//       <View style={styles.container}>
+//           <WebView
+//           source={{ uri }}
+//           javaScriptEnabled={true}
+//           style={styles.video}
+//           />
+//       </View>
+//       );
+//   }
+// }
